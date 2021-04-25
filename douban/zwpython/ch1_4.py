e61fb18cd64f25573d149159de869f6e82cc3294
yh0101 <yh0101@live.cn>
2021-04-25 13:21:30 +0800

    1

---
+++ douban/zwpython/ch1_4.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3 python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   ch1_4.py
+@Time    :   2021/04/25 12:57:32
+@Author  :   yh0101 
+@Contact :   yh0101@outlook.com
+@License :   (C)Copyright 2021-2025
+@Desc    :   None
+'''
+
+# here put the import lib
+from tkinter import *
+
+root = Tk()
+root.geometry("300x160+400+200")     # 距离屏幕左上角(400,200)
+root.mainloop()
